8488a4f19b712bbd69a97f4bccbbe3c5a313af93
Lodewic <lvantwillert.qelp@gmail.com>
2015-01-25 12:53:11 +0100

    Renamed my .R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this script will create a matrix and cache its
+## inverse.
 
-## Write a short comment describing this function
+## When the matrix is first created, the inverse will not
+## be calculated until cacheSolve() is called.
+## When cacheSolve() is called, the inverse is computed and returned.
+## If cacheSolve() is called on a matrix who's inverse was already computed,
+## the solution will be taken from the cache to reduce computational effort.
+
+
+# Take a matrix and create a matrix that is able to store its own inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## Create a matrix that can store its own inverse, assuming the matrix
+    ## does not change between calls
+    
+    # Initialize the inverse to NULL until it is set by the
+    # setinv() function
+    inv <<- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# Get the inverse-matrix from a matrix created by makeCacheMatrix().
+# If this function has already been called on a given matrix, then the
+# solution is taken from the cache instead of computing it again.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return the inverse-matrix of 'x'
+    
+    # Retrieve the cached inverse if it has been computed before.
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+        message('getting cached inverse')
+        return(inv)
+    }
+    
+    # Compute the inverse if it hasn't been computed before, and cache it
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setinv(inv)
+    inv
 }
+
+
+
+
+
+
+
+
+
+
